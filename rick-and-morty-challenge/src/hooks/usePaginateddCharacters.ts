4261ee66693b088f98985c70ec2b9cabb7304c6d
Tomas Amoretti <tom.ia@hotmail.com>
2025-09-25 20:14:26 -0300

    Setear selected caracter y acomodar componentes

--- rick-and-morty-challenge/src/hooks/usePaginateddCharacters.ts
+++ rick-and-morty-challenge/src/hooks/usePaginateddCharacters.ts
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react';
-import { fetchCharacters } from '../lib/rickAndMortyApiCall';
-import type { CharactersResponse } from '../types/rickAndMorty';
-
-export function usePaginatedCharacters(page: number) {
-  const [data, setData] = useState<CharactersResponse | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
-
-  useEffect(() => {
-    setLoading(true);
-    fetchCharacters(page)
-      .then((res) => {
-        setData(res);
-        setError(null);
-      })
-      .catch((err) => setError(err))
-      .finally(() => setLoading(false));
-  }, [page]);
-
-  return { data, loading, error };
-}
+import { useEffect, useState } from 'react'; 
+import { fetchCharacters } from '../lib/rickAndMortyApiCall'; 
+import type { CharactersResponse } from '../types/rickAndMorty'; 
+ 
+export function usePaginatedCharacters(page: number) { 
+  const [data, setData] = useState<CharactersResponse | null>(null); 
+  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState<Error | null>(null); 
+ 
+  useEffect(() => { 
+    setLoading(true); 
+    fetchCharacters(page) 
+      .then((res) => { 
+        setData(res); 
+        setError(null); 
+      }) 
+      .catch((err) => setError(err)) 
+      .finally(() => setLoading(false)); 
+  }, [page]); 
+ 
+  return { data, loading, error }; 
+} 
--- rick-and-morty-challenge/src/types/rickAndMorty.ts
+++ rick-and-morty-challenge/src/types/rickAndMorty.ts
@@ -1,17 +1,17 @@
-export type Character = {
-  id: number;
-  name: string;
-  status: string;
-  species: string;
-  image: string;
-};
-
-export type CharactersResponse = {
-  info: {
-    count: number;
-    pages: number;
-    next: string | null;
-    prev: string | null;
-  };
-  results: Character[];
-};
+export type Character = { 
+  id: number; 
+  name: string; 
+  status: string; 
+  species: string; 
+  image: string; 
+}; 
+ 
+export type CharactersResponse = { 
+  info: { 
+    count: number; 
+    pages: number; 
+    next: string | null; 
+    prev: string | null; 
+  }; 
+  results: Character[]; 
+}; 
